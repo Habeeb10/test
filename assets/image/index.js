c2c3428885dadaea8f4d76ffe0c25d2a82fe51bf
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-10-18 12:11:44 +0100

    feat: svg and png icons

---
+++ assets/image/index.js
@@ -0,0 +1,6 @@
+export const deola = require('./deola.png');
+export const abigail = require('./abigail.png');
+export const chi = require('./chi.png');
+export const vivian = require('./vivian.png');
+export const funmi = require('./funmi.png');
+export const grace = require('./grace.png');
